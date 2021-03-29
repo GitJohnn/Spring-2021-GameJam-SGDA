ed2a76f3fb1bf6e90125b14cc64b9c53737844a3
asmith564 <aaronsmith0709@gmail.com>
2021-03-29 18:39:55 -0500

    drag attempt

---
+++ Assets/BrokenPuzzleFolder/Scripts/Draggable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End");
+    }
+}
